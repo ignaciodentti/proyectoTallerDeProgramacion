d92f203a009fbae19c62abdd1f7e0055c78bc2a2
gastonmilano10 <42229747+gastonmilano10@users.noreply.github.com>
2020-12-01 22:48:00 -0300

    Modelo creado / Comienzo de definicion de repositorios e interfaces

---
+++ TPFinalTallerDeProgramacion/Modelo/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFinalTallerDeProgramacion.Modelo
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+                
+        }
+
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Segmento { get; set; }     //DEBERIA SER UNA CLASE ENUMERADA
+        public virtual IList<Tarjeta> listaTarjetas { get; set; }
+        public virtual IList<Cuenta> listaCuentas { get; set; }
+    }
+}
---
+++ TPFinalTallerDeProgramacion/Modelo/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFinalTallerDeProgramacion.Modelo
+{
+    public class Movimiento
+    {
+        public Movimiento()
+        {
+
+        }
+
+        public DateTime FechaHora { get; set; }
+        public string Descripcion { get; set; }
+        public virtual Cuenta Cuenta { get; set; }
+    }
+}
