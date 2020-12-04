c57c56e2cc779b97394819b7d3c7fce5b5d175e5
Alcides Rosano <accros_93@hotmail.com>
2020-12-04 01:16:31 -0300

    Inicio de pantallas
    Login

---
+++ TPFinalTallerDeProgramacion/Views/ViewLoginClient.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace TPFinalTallerDeProgramacion.Views
+{
+    partial class ViewLoginClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewLoginClient));
+            this.labelDNI = new System.Windows.Forms.Label();
+            this.labelPIN = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDNI = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.textBoxPIN = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDNI
+            // 
+            this.labelDNI.AutoSize = true;
+            this.labelDNI.BackColor = System.Drawing.Color.Transparent;
+            this.labelDNI.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDNI.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelDNI.Location = new System.Drawing.Point(83, 129);
+            this.labelDNI.Name = "labelDNI";
+            this.labelDNI.Size = new System.Drawing.Size(94, 51);
+            this.labelDNI.TabIndex = 1;
+            this.labelDNI.Text = "DNI";
+            this.labelDNI.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelPIN
+            // 
+            this.labelPIN.AutoSize = true;
+            this.labelPIN.BackColor = System.Drawing.Color.Transparent;
+            this.labelPIN.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPIN.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelPIN.Location = new System.Drawing.Point(87, 215);
+            this.labelPIN.Name = "labelPIN";
+            this.labelPIN.Size = new System.Drawing.Size(90, 51);
+            this.labelPIN.TabIndex = 2;
+            this.labelPIN.Text = "PIN";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(92, 282);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(92, 324);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(370, 1);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Location = new System.Drawing.Point(92, 373);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(370, 1);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Location = new System.Drawing.Point(92, 331);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(370, 1);
+            this.panel4.TabIndex = 6;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.FlatAppearance.BorderSize = 0;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClear.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClear.Image = global::TPFinalTallerDeProgramacion.Properties.Resources.icons8_delete_24;
+            this.buttonClear.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonClear.Location = new System.Drawing.Point(92, 334);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(370, 33);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Limpiar datos";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonLogin.Image = global::TPFinalTallerDeProgramacion.Properties.Resources.icons8_more_than_24;
+            this.buttonLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonLogin.Location = new System.Drawing.Point(92, 285);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(370, 33);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Ingresar";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.label1);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.DarkGreen;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.DarkGreen;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.LimeGreen;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.White;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(571, 48);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::TPFinalTallerDeProgramacion.Properties.Resources.icons8_back_50Whte;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(238, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxDNI.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.textBoxDNI.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxDNI.HintForeColor = System.Drawing.Color.Empty;
+            this.textBoxDNI.HintText = "";
+            this.textBoxDNI.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBoxDNI.isPassword = false;
+            this.textBoxDNI.LineFocusedColor = System.Drawing.Color.Blue;
+            this.textBoxDNI.LineIdleColor = System.Drawing.Color.Gray;
+            this.textBoxDNI.LineMouseHoverColor = System.Drawing.Color.Green;
+            this.textBoxDNI.LineThickness = 3;
+            this.textBoxDNI.Location = new System.Drawing.Point(168, 138);
+            this.textBoxDNI.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(294, 33);
+            this.textBoxDNI.TabIndex = 9;
+            this.textBoxDNI.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // textBoxPIN
+            // 
+            this.textBoxPIN.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxPIN.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.textBoxPIN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxPIN.HintForeColor = System.Drawing.Color.Empty;
+            this.textBoxPIN.HintText = "";
+            this.textBoxPIN.isPassword = false;
+            this.textBoxPIN.LineFocusedColor = System.Drawing.Color.Blue;
+            this.textBoxPIN.LineIdleColor = System.Drawing.Color.Gray;
+            this.textBoxPIN.LineMouseHoverColor = System.Drawing.Color.Green;
+            this.textBoxPIN.LineThickness = 3;
+            this.textBoxPIN.Location = new System.Drawing.Point(168, 224);
+            this.textBoxPIN.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPIN.Name = "textBoxPIN";
+            this.textBoxPIN.Size = new System.Drawing.Size(294, 33);
+            this.textBoxPIN.TabIndex = 10;
+            this.textBoxPIN.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // ViewLoginClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(571, 487);
+            this.Controls.Add(this.textBoxPIN);
+            this.Controls.Add(this.textBoxDNI);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelPIN);
+            this.Controls.Add(this.labelDNI);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewLoginClient";
+            this.Text = "ViewLoginClient";
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelDNI;
+        private System.Windows.Forms.Label labelPIN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panel4;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox textBoxDNI;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox textBoxPIN;
+    }
+}
---
+++ TPFinalTallerDeProgramacion/Views/ViewLoginClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPFinalTallerDeProgramacion.Views
+{
+    public partial class ViewLoginClient : Form
+    {
+        public ViewLoginClient()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxDNI_keyup(object sender, EventArgs e)
+        {
+            if (!Regex.Match(textBoxDNI.Text, "^[A-Z][a-zA-Z]*$").Success)
+            {
+                // last name was incorrect  
+                MessageBox.Show("Invalid last name", "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                lastNameTextBox.Focus();
+                return;
+            }
+        }
+    }
+}
